e06c7ed6f8e3e8098054f65ff243e44b24bdd39e
mecva <melanie.echevarria@etu.univ-tlse2.fr>
2025-01-09 14:58:01 +0000

    #5.4.3 début code question , à reprendre (interruption)

--- Depot_HLM_M2_SIGMA/scripts/sample_analysis_spectral_variability.py
+++ Depot_HLM_M2_SIGMA/scripts/sample_analysis_spectral_variability.py
@@ -1,10 +1,12 @@
 import sys
-sys.path.append('/home/onyxia/work/libsigma')
 import os
 import geopandas as gpd
+import rasterio
 from sklearn.model_selection import train_test_split
 # personal librairies
 import classification as cla
+
+sys.path.append('/home/onyxia/work/libsigma')
 
 # 1 --- define parameters
 # inputs
@@ -17,12 +19,45 @@ output_file_violin = os.path.join(output_folder, 'violin_plot_dist_centroide_by_
 
 # Chargement des couches
 sample_data = gpd.read_file(sample_filename)
+ndvi_open = rasterio.open(image_filename)
 
 # Défintion des classes rouges et bleues
 classe_rouge = ['11', '12', '13', '14', '23', '24', '25']
 classe_bleue = ['15', '26', '28', '29']
 
-# Sélection des polygones correspondant aux classes rouges et bleues dans deux variables distinctes
+# Sélection des polygones correspondant aux classes rouges et bleues dans deux variables
 sample_rouge = sample_data[sample_data['Code'].isin(classe_rouge)]
 sample_bleu = sample_data[sample_data['Code'].isin(classe_bleue)]
 
+########## --- À l'échelle de l'image entière, tous polygones confondus --- ##########
+# Regrouper les pixels appartenant à chaque classe
+###A REPRENDRE###
+import numpy as np
+import rasterio
+from rasterio.mask import mask
+
+# Fonction pour extraire les pixels NDVI pour un groupe de polygones
+def extract_ndvi_values(sample_group, raster, affine):
+    # Convertir les polygones en géométrie GeoJSON
+    geometries = [geom for geom in sample_group.geometry]
+    
+    # Extraire les pixels NDVI à l'intérieur des polygones
+    out_image, out_transform = mask(raster, geometries, crop=True)
+    
+    # Remettre en tableau 2D pour faciliter l'analyse
+    ndvi_values = out_image[0].flatten()
+    
+    # Filtrer les valeurs valides (par exemple, en excluant les nodata)
+    ndvi_values = ndvi_values[ndvi_values != raster.nodata]
+    
+    return ndvi_values
+
+# Extraction des valeurs NDVI pour les classes rouges
+ndvi_rouge = extract_ndvi_values(sample_rouge, ndvi_open, ndvi_open.transform)
+
+# Extraction des valeurs NDVI pour les classes bleues
+ndvi_bleu = extract_ndvi_values(sample_bleu, ndvi_open, ndvi_open.transform)
+
+# Affichage des résultats
+print("NDVI pour les classes rouges :", ndvi_rouge)
+print("NDVI pour les classes bleues :", ndvi_bleu)
